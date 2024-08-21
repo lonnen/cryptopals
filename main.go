d323cb171b6e12ddd4b0ddfa89b8556d7212dc87
lonnen <chris.lonnen@gmail.com>
2024-08-20 22:56:39 -0700

    hexToBase64 implementation

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
+import (
+	"encoding/base64"
+	"encoding/hex"
+)
+
 func main() {
+}
+
+func hexToBase64(i string) (string, error) {
+	bytes, err := hex.DecodeString(i)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(bytes), nil
 }
