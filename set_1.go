2718c4f7dc633be890630df61313c7f5cf1c9e5b
lonnen <chris.lonnen@gmail.com>
2024-08-29 12:43:16 -0700

    improve error logging in set 1 challenge 2

--- set_1.go
+++ set_1.go
@@ -1,14 +1,22 @@
 package cryptopals
 
+import (
+	"encoding/hex"
+	"log"
+)
+
 func Set1Challenge1(i string) string {
 	b64, _ := hexToBase64(i)
 	return b64
 }
 
 func Set1Challenge2(p string, q string) string {
-	decoded_p, _ := hexToBase64(p)
-	decoded_q, _ := hexToBase64(q)
-	decoded, _ := xor([]byte(decoded_p), []byte(decoded_q))
+	decoded_p, _ := hex.DecodeString(p)
+	decoded_q, _ := hex.DecodeString(q)
+	decoded, e := xor([]byte(decoded_p), []byte(decoded_q))
+	if e != nil {
+		log.Printf("Error: %q", e)
+	}
 	return string(decoded)
 }
 
