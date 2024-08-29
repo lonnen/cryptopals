a33b027934d8a47976d7c2c6aa14b5a784c84231
lonnen <chris.lonnen@gmail.com>
2024-08-29 13:06:19 -0700

    solve challenge 2
    previously set1challenge2 was returning the ascii encoded string, when we should try and stay in hex encoded space

--- set_1.go
+++ set_1.go
@@ -13,11 +13,13 @@ func Set1Challenge1(i string) string {
 func Set1Challenge2(p string, q string) string {
 	decoded_p, _ := hex.DecodeString(p)
 	decoded_q, _ := hex.DecodeString(q)
-	decoded, e := xor([]byte(decoded_p), []byte(decoded_q))
+	decoded, e := xor(decoded_p, decoded_q)
 	if e != nil {
 		log.Printf("Error: %q", e)
 	}
-	return string(decoded)
+	reencoded := make([]byte, hex.EncodedLen(len(decoded)))
+	hex.Encode(reencoded, decoded)
+	return string(reencoded)
 }
 
 func Set1Challenge3(hexEncoded string) (string, string) {
