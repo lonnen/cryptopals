eb3baa472eab64dd000053d7f1607b38c75e0dad
lonnen <chris.lonnen@gmail.com>
2024-09-06 14:43:11 -0700

    add method for reversing single byte xor cyphers
    the 1 byte keyspace is small enough that we can brute force it. Try all 255 bytes, and for each calculate the log probability of each unigram (using a minimum value of -100 to prevent infinities from making it *weird*). Then sum those log-probs together. The byte resulting in the highest sum string is probably the key

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"errors"
+	"math"
+	"strings"
 )
 
 func hexToBase64(i string) (string, error) {
@@ -38,4 +40,71 @@ func xorCipherStrings(left string, right string) (string, error) {
 	reencoded := make([]byte, hex.EncodedLen(len(decoded)))
 	hex.Encode(reencoded, decoded)
 	return string(reencoded), e
+}
+
+func xorCipherSingleByte(cipher []byte, key byte) []byte {
+	result := make([]byte, len(cipher))
+	for i := 0; i < len(cipher); i++ {
+		result[i] = cipher[i] ^ key
+	}
+	return result
+}
+
+// source: https://github.com/piersy/ascii-char-frequency-english/blob/main/ascii_freq.txt
+var englishLetterFrequency = map[string]float64{
+	"a": 11.7,
+	"b": 4.4,
+	"c": 5.2,
+	"d": 3.2,
+	"e": 2.8,
+	"f": 4,
+	"g": 1.6,
+	"h": 4.2,
+	"i": 7.3,
+	"j": 0.51,
+	"k": 0.86,
+	"l": 2.4,
+	"m": 3.8,
+	"n": 2.3,
+	"o": 7.6,
+	"p": 4.3,
+	"q": 0.22,
+	"r": 2.8,
+	"s": 6.7,
+	"t": 16,
+	"u": 1.2,
+	"v": 0.82,
+	"w": 5.5,
+	"x": 0.045,
+	"y": 0.76,
+	"z": 0.045,
+}
+
+func isItEnglish(buffer []byte) float64 {
+	score := 0.0
+	for _, b := range buffer {
+		score += math.Max(math.Log(englishLetterFrequency[strings.ToLower(string(b))]), -100.0)
+	}
+	return score
+}
+
+func findSingleByteXOR(hexed string) (string, byte) {
+	decoded_hexed, _ := hex.DecodeString(hexed)
+
+	bestKey := byte(0)
+	bestScore := math.Inf(-1)
+	bestMessage := ""
+	for b := range 256 {
+		key := byte(b)
+		decoded := xorCipherSingleByte(decoded_hexed, key)
+		score := isItEnglish(decoded)
+
+		if score > bestScore {
+			bestScore = score
+			bestKey = key
+			bestMessage = string(decoded)
+		}
+	}
+
+	return bestMessage, bestKey
 }
