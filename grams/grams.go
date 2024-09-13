ffc494b3042fa50bef8eeffba96d47476d215d19
lonnen <chris.lonnen@gmail.com>
2024-09-12 23:53:28 -0700

    stub out grams function
    this will eventually load and parse files containing information about letter frequency in english. these files will be included in the binary via embed.FS

--- grams/grams.go
+++ grams/grams.go
@@ -2,6 +2,9 @@ package grams
 
 import (
 	"embed"
+	"log"
+	"math"
+	"strconv"
 )
 
 //go:embed english
@@ -12,6 +15,44 @@ var files = map[string]string{
 	"bigrams":   "english_bigrams.txt",
 	"trigrams":  "english_trigrams.txt",
 	"quadgrams": "english_quadgrams.txt",
+}
+
+func grams(count string) {
+	filename := files[count]
+	f, err := english.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var gramCounts = make(map[string]float64)
+	var gramCount = 1 // get this from a map or enum?
+	var total = 0.0
+	// parse file into string: int
+	i := 0
+	j := 0
+	for i < len(f) {
+		// count-length collection of letters
+		gram := f[i : i+gramCount]
+		// advance the pointer past the letters and space
+		i += gramCount + 1
+		// grab the number
+		j = i
+		for f[i] != 10 {
+			i++
+		}
+		c := f[j:i]
+
+		if err != nil {
+			log.Fatal("Error while parsing file")
+		}
+		g := string(gram)
+		gramCounts[g], _ = strconv.ParseFloat(string(c), 64)
+	}
+
+	for g, c := range gramCounts {
+		gramCounts[g] = math.Log(c / total)
+	}
+
 }
 
 func chunk(text string, size int) []string {
