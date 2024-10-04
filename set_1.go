1c6c08ca98e58fb0391b758c80e5264aa8b4dff3
lonnen <chris.lonnen@gmail.com>
2024-10-03 23:55:00 -0700

    closer to victory, but still infinitely far away
    this completes the sketch of the function. the answer is still incorrect, and unexpectedly there's no best key. More investigation is necessary.

--- set_1.go
+++ set_1.go
@@ -2,6 +2,7 @@ package cryptopals
 
 import (
 	"encoding/hex"
+	"fmt"
 	"math"
 	"slices"
 	"strings"
@@ -69,8 +70,11 @@ func Set1Challenge6(plaintext string, key string) string {
 		normalizedDistance := totalDistance
 		keyDistances[keySize] = normalizedDistance
 	}
-
 	// the KEYSIZE with the smallest normalized hamming distance is probably the key
+
+	bestLine := ""
+	bestKey := ""
+	bestCost := math.Inf(-1)
 
 	// break the ciphertext into blocks of KEYSIZE length
 	keySize := 3
@@ -81,18 +85,33 @@ func Set1Challenge6(plaintext string, key string) string {
 	chunkCount := len(chunks)
 
 	println(chunkCount)
-	// for i := range chunks {
-	// 	//println(i)
-	// }
 
 	// transpose the blocks (a block of the first byte of each block, then the second byte of each block, etc)
 	transposed := transpose(chunks)
 	println(len(transposed))
 
 	// solve each block as single-character XOR
-	// for each block, the single-byte XOR key that produces the best histogram is the key for that block
-	decrypted := hex.EncodeToString(repeatingKeyXOR([]byte(plaintext), []byte(key)))
+	keys := []byte{}
+	totalCost := math.Inf(-1)
+	for b := range transposed {
+		block := transposed[b]
+		// for each block, the single-byte XOR key that produces the
+		// best letter frequency distribution is the key for that block
+		_, key, cost := findSingleByteXOR(block)
+		keys = append(keys, key)
+		totalCost += cost
+	}
+
+	if totalCost > bestCost {
+		bestKey = string(keys)
+		bestCost = totalCost
+		bestLine = string(repeatingKeyXOR(cipherText, keys))
+		println("NEW BEST")
+		fmt.Printf("key: %s, cost: %f, line: %s", bestKey, bestCost, bestLine)
+	}
+
+	fmt.Printf("key: %s", string(bestKey))
 
 	// put the keys together for all the blocks to get the key
-	return decrypted
+	return bestLine
 }
